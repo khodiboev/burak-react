567cdd027b55087368578b8200678661b36d3200
Victor <khadibayevjurabek@gmail.com>
2026-03-13 16:56:24 +0900

    feat: pupularDishes

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -9,14 +9,23 @@ import CardOverflow from "@mui/joy/CardOverflow";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
-const list = [
-  { productName: "Lavash", imagePath: "/img/lavash.webp"},
-  { productName: "Cutlet", imagePath: "/img/cutlet.webp"},
-  { productName: "Kebab", imagePath: "/img/kebab.webp"},
-  { productName: "Kebab", imagePath: "/img/kebab-fresh.webp"},
-]
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+
+/** REDUX SLICE & SELECTOR */
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes }),
+);
 
 export default function PopularDishes() {
+  const { popularDishes } = useSelector(popularDishesRetriever);
+
+    console.log("pupularDishes:", popularDishes)
+
   return (
     <div className="popular-dishes-frame">
       <Container>
@@ -24,41 +33,45 @@ export default function PopularDishes() {
           <Box className="category-title">Popular Dishes</Box>
 
           <Stack className="cards-frame">
-            {list.length !== 0 ? (
-              list.map((ele, index) => {
+            {popularDishes.length !== 0 ? (
+              popularDishes.map((ele: Product) => {
+                const imagePath = `${serverApi}/${ele.productImages[0]}`
                 return (
-                  <CssVarsProvider key={index}>
+                  <CssVarsProvider key={ele._id}>
                     <Card className={"card"}>
                       <CardCover>
-                        <img src={ele.imagePath} alt="" />
+                        <img src={imagePath} alt="" />
                       </CardCover>
                       <CardCover className={"card-cover"} />
                       <CardContent sx={{ justifyContent: "flex-end" }}>
-                        <Stack 
+                        <Stack
                           flexDirection={"row"}
                           justifyContent={"space-between"}
-                          >
+                        >
                           <Typography
                             level="h2"
                             fontSize="lg"
                             textColor="#fff"
-                            mb={1}>
-                              {ele.productName}
-                            </Typography>
-                            <Typography
-                              sx={{
-                                fontWeight: "md",
-                                color: "neutral.300",
-                                alignItems: "center",
-                                display: "flex",
-                              }}>
-                                20
-                                <VisibilityIcon
-                                  sx={{fontSize:25, marginLeft: "5px"}} />
-                              </Typography>
+                            mb={1}
+                          >
+                            {ele.productName}
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontWeight: "md",
+                              color: "neutral.300",
+                              alignItems: "center",
+                              display: "flex",
+                            }}
+                          >
+                            {ele.productViews}
+                            <VisibilityIcon
+                              sx={{ fontSize: 25, marginLeft: "5px" }}
+                            />
+                          </Typography>
                         </Stack>
                       </CardContent>
-                      
+
                       <CardOverflow
                         sx={{
                           display: "flex",
@@ -67,20 +80,23 @@ export default function PopularDishes() {
                           px: "var(--Card-padding)",
                           borderTop: "1px solid",
                           height: "60px",
-                        }}>
-                          <Typography
-                            startDecorator={<DescriptionOutlinedIcon />}
-                            textColor="neutral.300">
-                              This is Delicious Meal
-                            </Typography>
-                        </CardOverflow>
-
+                        }}
+                      >
+                        <Typography
+                          startDecorator={<DescriptionOutlinedIcon />}
+                          textColor="neutral.300"
+                        >
+                        ${ele.productDesc}
+                        </Typography>
+                      </CardOverflow>
                     </Card>
                   </CssVarsProvider>
                 );
               })
-            ):(
-              <Box className="no-data">Popular products are not available now.</Box>
+            ) : (
+              <Box className="no-data">
+                Popular products are not available now.
+              </Box>
             )}
           </Stack>
         </Stack>
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,24 +7,39 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatcher = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
-const popularDishesRetriever = createSelector(retrievePopularDishes, (popularDishes) => ({popularDishes}));
 
 export default function HomePage() {
-  const {setPopularDishes} = actionDispatcher(useDispatch());
-  const {popularDishes} = useSelector(popularDishesRetriever);
+  const { setPopularDishes } = actionDispatcher(useDispatch());
 
-  useEffect(() => {}, []);
+  console.log(process.env.REACT_APP_API_URL);
+
+  useEffect(() => {
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productView",
+        productCollection: ProductCollection.DISH,
+      })
+      .then(data => {
+        console.log("data passed here:", data)
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
 
   return (
     <div className={"homepage"}>
