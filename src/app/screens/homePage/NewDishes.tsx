9bfef0890ca985ab08bb5961015c076a9e7cd74c
Victor <khadibayevjurabek@gmail.com>
2026-03-13 17:40:44 +0900

    feat: newDishes

--- src/app/screens/homePage/NewDishes.tsx
+++ src/app/screens/homePage/NewDishes.tsx
@@ -7,31 +7,40 @@ import Typography from "@mui/joy/Typography";
 import { CssVarsProvider } from "@mui/joy/styles";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import Divider from "../../components/divider";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveNewDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
-const newDishes = [
-  {productName: "Lavash", imagePath: "/img/lavash.webp"},
-  {productName: "Cutlet", imagePath: "/img/cutlet.webp"},
-  {productName: "Kebab", imagePath: "/img/kebab.webp"}, 
-  {productName: "Kebab", imagePath: "/img/kebab-fresh.webp"},
-];
+/** REDUX SLICE & SELECTOR */
+const newDishesRetriever = createSelector(retrieveNewDishes, (newDishes) => ({
+  newDishes,
+}));
 
 export default function NewDishes() {
+  const { newDishes } = useSelector(newDishesRetriever);
+
+  console.log("newDishes:", newDishes);
+
   return (
     <div className={"new-product-frame"}>
-       <Container>
-          <Stack className={"main"}>
-            <Box className={"category-title"}>Fresh Menu</Box>
-            <Stack className={"cards-frame"}>
-
-              <CssVarsProvider>
-                {newDishes.length !== 0 ? (
-                  newDishes.map((ele, index) => {
-                  return(
-                    <Card key={index} variant="outlined" className={"card"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Fresh Menu</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {newDishes.length !== 0 ? (
+                newDishes.map((product: Product) => {
+                  const imagePath = `${serverApi}/${product.productImages[0]}`;
+                  const sizeVolume = product.productCollection === ProductCollection.DISH ? product.productVolume + "l" : product.productSize + " size";
+                  return (
+                    <Card key={product._id} variant="outlined" className={"card"}>
                       <CardOverflow>
-                        <div className="product-sale">Normal size</div>
+                        <div className="product-sale">{sizeVolume}</div>
                         <AspectRatio ratio="1">
-                          <img src={ele.imagePath} alt="" />
+                          <img src={imagePath} alt="" />
                         </AspectRatio>
                       </CardOverflow>
 
@@ -39,37 +48,36 @@ export default function NewDishes() {
                         <Stack className="info">
                           <Stack flexDirection={"row"}>
                             <Typography className={"title"}>
-                              {ele.productName}
+                              {product.productName}
                             </Typography>
 
-                          <Divider width="2" height="24" bg="#d9d9d9"/>
+                            <Divider width="2" height="24" bg="#d9d9d9" />
 
-                            <Typography className={"price"}>$12</Typography>
+                            <Typography className={"price"}>$1{product.productPrice}</Typography>
                           </Stack>
 
                           <Stack>
                             <Typography className={"views"}>
-                              20
+                              {product.productViews}
                               <VisibilityIcon
-                                sx={{fontSize:20, marginLeft: "5px"}} />
+                                sx={{ fontSize: 20, marginLeft: "5px" }}
+                              />
                             </Typography>
                           </Stack>
-                          
                         </Stack>
                       </CardOverflow>
                     </Card>
                   );
                 })
-                ):(
-                    <Box className="no-data">New products are not available now.</Box>
-                )}
-              </CssVarsProvider>
-
-            </Stack>
+              ) : (
+                <Box className="no-data">
+                  New products are not available now.
+                </Box>
+              )}
+            </CssVarsProvider>
           </Stack>
-       </Container>
+        </Stack>
+      </Container>
     </div>
   );
 }
-
-
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,22 +5,22 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatcher = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatcher(useDispatch());
+  const { setPopularDishes, setNewDishes } = actionDispatcher(useDispatch());
 
   console.log(process.env.REACT_APP_API_URL);
 
@@ -33,13 +33,23 @@ export default function HomePage() {
         order: "productView",
         productCollection: ProductCollection.DISH,
       })
-      .then(data => {
-        console.log("data passed here:", data)
+      .then((data) => {
         setPopularDishes(data);
       })
       .catch((err) => console.log(err));
-  }, []);
 
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setNewDishes(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={"homepage"}>
